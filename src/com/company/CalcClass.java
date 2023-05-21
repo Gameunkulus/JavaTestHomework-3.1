ae82c15c340680894e896fd910b2d036c10f750a
Gameunkulus <bugr24-8@mail.ru>
2023-05-21 13:42:39 +0300

    program redacted 3

--- src/com/company/CalcClass.java
+++ src/com/company/CalcClass.java
@@ -1,9 +1,9 @@
 package com.company;
 
-public class calcClass {
-    public static int calculate(int price){
+public class CalcClass {
+    public static int calculate(int price) {
         int bonus = 0;
-        if(price > 20){
+        if (price > 20) {
             bonus = price / 20;
         }
         return bonus;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        calcClass service = new calcClass();
+        CalcClass service = new CalcClass();
         int price = 10_000;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
